f47f31437fae6177c72ec5d98954970da613f6f0
dbird07 <dylanbird20@gmail.com>
2025-10-03 16:45:58 -0400

    so

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,5 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
-    '@babel/plugin-syntax-flow',
-    '@babel/plugin-transform-flow-strip-types',
-  ],
-  overrides: [
-    {
-      test: /node_modules\/react-native\/.*\.js$/,
-      plugins: [
-        '@babel/plugin-transform-flow-strip-types',
-        '@babel/plugin-syntax-flow'
-      ],
-    },
   ],
 };
